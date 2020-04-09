4a6ace42679ad34a110ff923771b77e57cbf6344
Slava <kreeon@ya.ru>
2020-04-09 18:34:12 +0300

    add Mappable interface

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,5 +1,11 @@
-import { User } from './User';
-import { Company } from './Company';
+export interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+  markerContent(): string;
+  color: string;
+}
 
 export class CustomMap {
   private googleMap: google.maps.Map;
@@ -14,12 +20,18 @@ export class CustomMap {
     });
   }
 
-  addUserMarker(user: User): void {
-    new google.maps.Marker({
+  addMarker(mappable: Mappable): void {
+    const marker = new google.maps.Marker({
       map: this.googleMap,
-      position: user.location,
+      position: mappable.location,
+    });
+
+    marker.addListener('click', () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: mappable.markerContent(),
+      });
+
+      infoWindow.open(this.googleMap, marker);
     });
   }
-
-  addCompanyMarker(company: Company): void {}
 }
